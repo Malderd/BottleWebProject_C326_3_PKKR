54142b6d9083e6562d0b7515a5575947f4f56de9
viruy04 <annakazakow4@yandex.ru>
2026-06-04 09:54:22 +0300

    изменение в скролле в селениум: js заменён на python

--- BottleWebProject_C326_3_PKKR/tests/test_cliques_selenium.py
+++ BottleWebProject_C326_3_PKKR/tests/test_cliques_selenium.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
 
 
 class TestCliquesSelenium(unittest.TestCase):
@@ -12,7 +14,7 @@ class TestCliquesSelenium(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         print("ЗАПУСК ТЕСТОВ ПС")
-        cls.driver = webdriver.Chrome() # Инициализация браузера Chrome
+        cls.driver = webdriver.Edge() # Инициализация браузера
         cls.driver.maximize_window() # Разворачиваем окно на весь экран
         cls.base_url = "http://127.0.0.1:5555" # Наш url
         cls.wait = WebDriverWait(cls.driver, 10) # Используется для ожидания загрузки элементов
@@ -20,25 +22,25 @@ class TestCliquesSelenium(unittest.TestCase):
     # Прокрутка вниз для ввода параметров
     def scroll_up(self, y=230):
         hero = self.driver.find_element(By.CLASS_NAME, "hero-cliques")
-        self.driver.execute_script("arguments[0].scrollTop += arguments[1];", hero, y)
+        origin = ScrollOrigin.from_element(hero) # Начинаем скрол от элемента
+        ActionChains(self.driver).scroll_from_origin(origin, 0, y).perform() # Скролл на y пикселей
         time.sleep(1.5)
-
+ 
     # Прокрутка вниз для результатов
     def scroll_down(self, y=250):
         hero = self.driver.find_element(By.CLASS_NAME, "hero-cliques")
-        self.driver.execute_script("arguments[0].scrollTop += arguments[1];", hero, y)
+        origin = ScrollOrigin.from_element(hero)
+        ActionChains(self.driver).scroll_from_origin(origin, 0, y).perform()
         time.sleep(1.5)
-
+ 
     # Прокрутка вверх
     def scroll_back(self, y=250):
         hero = self.driver.find_element(By.CLASS_NAME, "hero-cliques")
-        self.driver.execute_script(
-        "arguments[0].scrollTop -= arguments[1];", # -y
-         hero, y
-        )  
+        origin = ScrollOrigin.from_element(hero)
+        ActionChains(self.driver).scroll_from_origin(origin, 0, -y).perform() # -y
         time.sleep(1.5)
 
-    # Ожидание
+    # Ожидание с выводом в консоль
     def pause(self, text, sec=None):
         sec = sec or random.uniform(1,2)
         print(text)
@@ -48,19 +50,19 @@ class TestCliquesSelenium(unittest.TestCase):
     def test_1_open_page(self):
         print("\n[TEST 1] Старт с главной страницы")
         self.driver.get(self.base_url) # Главная страница
-        self.pause("[TEST 1] Ждём главную страницу")
+        time.sleep(1)
         self.driver.get(self.base_url + "/clique_detection?tab=manual") # Переход на нашу страницу
-        self.pause("[TEST 1] Переход на manual")
+        time.sleep(1)
         self.wait.until(
             EC.presence_of_element_located((By.TAG_NAME, "body"))
         )
-        print("TEST 1 пройден") # Информация в консоль об успехе
+        print("\nTEST 1 пройден") # Информация в консоль об успехе
 
     # ПС-02, случайный граф, его отрисовка и сохранение
     def test_2_random_generation(self):
         print("\n[TEST 2] RANDOM сценарий")
         self.driver.get(self.base_url + "/clique_detection?tab=random")
-        self.pause("[TEST 2] Открыли random")
+        time.sleep(1)
         self.scroll_up() # Скролл вниз
         n_input = self.wait.until(
             EC.presence_of_element_located((By.NAME, "n_size")) # Нахождение поля по имени в html
@@ -72,13 +74,13 @@ class TestCliquesSelenium(unittest.TestCase):
         # Ввод данных
         n_input.send_keys("5")
         density_input.send_keys("60")
-        self.pause("[TEST 2] Ввели данные")
+        time.sleep(1)
 
         btn = self.wait.until(
             EC.element_to_be_clickable((By.ID, "btn-create-matrix"))
         )
         btn.click() # Создаём матрицу нажатием на кнопку
-        self.pause("[TEST 2] Сгенерировали матрицу")
+        time.sleep(1)
 
         self.wait.until(
             EC.presence_of_element_located((By.ID, "matrix-form")) # Ждём появления матрицы на странице
@@ -87,10 +89,10 @@ class TestCliquesSelenium(unittest.TestCase):
         solve_btn = self.wait.until(
             EC.element_to_be_clickable((By.ID, "btn-solve"))
         )
-        self.pause("[TEST 2] Жмём построить граф")
+        time.sleep(1)
         solve_btn.click() # Строим граф нажатием кнопки
 
-        self.pause("[TEST 2] Ждём граф")
+        time.sleep(1)
         self.wait.until(
             EC.presence_of_element_located((By.CSS_SELECTOR, ".graph-image")) # Ждём появления изображения на странице
         )
@@ -100,17 +102,17 @@ class TestCliquesSelenium(unittest.TestCase):
             EC.element_to_be_clickable((By.ID, "btn-save"))
         )
         self.scroll_back() # Скролл вверх
-        self.pause("[TEST 2] Жмём сохранить")
+        time.sleep(1)
         save_btn.click() # Сохранение
 
-        self.pause("[TEST 2] Готово")
+        time.sleep(1)
         print("TEST 2 пройден")
 
     # ПС-03, выгрузка из файла + построение графа
     def test_3_file_upload(self):
         print("\n[TEST 3] FILE сценарий")
         self.driver.get(self.base_url + "/clique_detection?tab=file")
-        self.pause("[TEST 3] Открыли file вкладку")
+        time.sleep(1)
 
         # Неверный файл
         error_file = r"C:\Users\annak\OneDrive\Рабочий стол\error.txt"
@@ -154,7 +156,6 @@ class TestCliquesSelenium(unittest.TestCase):
         upload_btn.click()
 
         time.sleep(2)
-        self.pause("[TEST 3] Загрузили матрицу")
 
         self.wait.until(
             EC.presence_of_element_located((By.CLASS_NAME, "matrix-table")) # Ждём появления матрицы на странице
@@ -163,10 +164,10 @@ class TestCliquesSelenium(unittest.TestCase):
         solve_btn = self.wait.until(
             EC.element_to_be_clickable((By.ID, "btn-solve"))
         )
-        self.pause("[TEST 3] Строим граф")
+        time.sleep(1)
         solve_btn.click() # Построение графа
 
-        self.pause("[TEST 3] Ждём результат")
+        time.sleep(1)
         self.wait.until(
             EC.presence_of_element_located((By.CSS_SELECTOR, ".graph-image")) # Ждём появления изображения на странице
         )
